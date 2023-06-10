8aaa92fef57cc25ec9a14541163ae629c65f7fa9
IdR3n <bidrisarikaya@gmail.com>
2023-06-10 10:56:26 +0200

    it's a laittle bit confused... I need ask someone

--- main.js
+++ main.js
@@ -1,30 +1,3 @@
-//document.addEventListener('DOMContentLoaded', function() {
-//    var calendarEl = document.getElementById('calendar');
-//    var calendar = new FullCalendar.Calendar(calendarEl, {
-//      initialView: 'dayGridMonth'
-//    });
-//    calendar.render();
-//  });
-//
-//  async function getHoliday() {
-//    try{
-//        const response = await axios.get('https://date.nager.at/api/v3/publicholidays/2023/CH');
-//        const holidayList = response.data
-//        const holElement = document.getElementById('holidayList');
-//        console.log(response.data)
-//        for (let holiday of holidayList) {
-//            const holEl = document.createElement('div')
-//            holEl.classList.add('col')
-//            holEl.addEventListener('click', function(){
-//                alert(holiday.date)
-//            })
-//            holElement.appendChild(holEl)
-//        }
-//    } catch(e){
-//        console.error(e);
-//    }
-//  }
-//  getHoliday();
 
 document.addEventListener('DOMContentLoaded', async function getFetes() {
     try {
@@ -63,31 +36,26 @@ document.addEventListener('DOMContentLoaded', async function getFetes() {
     } catch (e) {
         throw e;
     }
+    //check this out...
+    
+    //--------------------------------------------------------------------------------------------------//
+    //let mButton = document.getElementById("mButton");
+    //mButton = [];
+    //
+    ////iki defa listener var birisini kaldirmam gerekiyor..
+    //for (let mdlbttn of mButton) {
+    //mdlbttn = document.addEventListener('click', function () {
+    //    
+    //    modal.createElement("div");
+    //
+    // mdlbttn.classList.add("fc-event-title-container");
+    //});
+    //}
+//--------------------------------------------------------------------------------------------------//
 
+    // I need eventListener -> when I click it must be open a modal for add new holiday in the date the i clicked(nessesaire)...
 
-    // I need eventListener -> when I click it must be open a modal for add new holiday in the date the i clicked...
-
-    //nouvelle Modal doit faire dans le html pour comprendre mieux utilisation de JavaScript...
-
-//    <div class="modal" tabindex="-1" role="dialog">
-//  <div class="modal-dialog" role="document">
-//    <div class="modal-content">
-//      <div class="modal-header">
-//        <h5 class="modal-title">Modal title</h5>
-//        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-//          <span aria-hidden="true">&times;</span>
-//        </button>
-//      </div>
-//      <div class="modal-body">
-//        <p>Modal body text goes here.</p>
-//      </div>
-//      <div class="modal-footer">
-//        <button type="button" class="btn btn-primary">Save changes</button>
-//        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-//      </div>
-//    </div>
-//  </div>
-//</div>
+    //nouvelle Modal doit faire dans le html pour comprendre mieux utilisation de JavaScript(nessesaire)...
 
 //Here is for add a new holiday in our agenda...
 
